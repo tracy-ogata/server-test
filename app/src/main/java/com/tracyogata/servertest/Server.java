c728ce6c63fb059c22f80cfb90c5d1a68895bac3
Tracy <tracy.ogata125@gmail.com>
2020-03-19 11:20:23 -0700

    First Commit

---
+++ app/src/main/java/com/tracyogata/servertest/Server.java
@@ -0,0 +1,170 @@
+package com.tracyogata.servertest;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+
+/**
+ * Created by farna on 2/22/2018.
+ */
+
+// ADDED FROM SERVER
+
+
+public class Server {
+    MainActivity activity;
+    ServerSocket serverSocket;
+    // original
+    String message = "";
+    String msgReply ="";
+    static final int socketServerPORT = 8081;
+    int mission=555;
+
+    public Server(MainActivity activity, String msgReply) {
+        this.activity = activity;
+        this.msgReply = msgReply;
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+    }
+    public int getPort() {
+        return socketServerPORT;
+    }
+
+    public void onDestroy() {
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SocketServerThread extends Thread {
+
+        int count = 0;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(socketServerPORT);
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    count++;
+                    message = ""; /*+= "#" + count + " from "
+                            + socket.getInetAddress() + ":"
+                            + socket.getPort() + "\n";*/
+
+                    activity.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+//                            activity.msg.setText(message);
+                        }
+                    });
+
+                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(
+                            socket, count);
+                    socketServerReplyThread.run();
+
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private class SocketServerReplyThread extends Thread {
+
+        private Socket hostThreadSocket;
+        int cnt;
+
+
+        SocketServerReplyThread(Socket socket, int c) {
+            hostThreadSocket = socket;
+            cnt = c;
+        }
+
+        @Override
+        public void run() {
+
+            OutputStream outputStream;
+            //"HELLO FROM MASTER";
+            //"Hello from Master,send me your info, you are #" + cnt +"\n"; ////////////////////////////////////////////////////////////////
+
+            try {
+                outputStream = hostThreadSocket.getOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                printStream.print(msgReply + "mission #: " + mission);
+                printStream.close();
+
+                Log.i("SERVRER","server" + msgReply);
+
+                message = "";//"Sent Text to black phone";
+                activity.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // activity.msg.setText(message);
+                    }
+                });
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                message += "Something wrong! " + e.toString() + "\n";
+            }
+
+            activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    // activity.msg.setText(message);
+                }
+            });
+        }
+
+    }
+
+    public String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress
+                            .nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "Server running at : "
+                                + inetAddress.getHostAddress();
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+        return ip;
+    }
+}
